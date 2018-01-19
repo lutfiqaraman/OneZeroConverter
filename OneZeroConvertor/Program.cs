0fce8227c7eff9fe1077e3be3e2d00da9409ff42
lutfiqaraman <lutfiqaraman@gmail.com>
2018-01-19 20:15:38 +0200

    Done

--- OneZeroConvertor/Program.cs
+++ OneZeroConvertor/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OneZeroConvertor
+namespace OneZeroConverter
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] binarynumbers = { 1, 1, 1, 1 };
+            int result = binaryArrayToNumber(binarynumbers);
+
+            Console.Write(result);
+            Console.ReadLine();
+        }
+
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            int number = 0;
+            Array.Reverse(BinaryArray);
+
+
+            for (int i = 0; i < BinaryArray.Length; i++)
+                number += (int)(BinaryArray[i] * Math.Pow(2, i));
+
+            return number;
         }
     }
 }
